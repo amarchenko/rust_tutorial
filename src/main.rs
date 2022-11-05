b690affc1bf60da7f4ae71ef9276b541e2af8083
Aleksei Marchenko <amarchenko@roku.com>
2022-11-04 17:20:55 -0700

    Added several samples inclding 'rand' usage.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,49 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
+    let random_num: i32 = rand::thread_rng().gen_range(1..101);
+    println!("Random num: {}", random_num);
+}
+
+fn sample4_main() {
+    let is_true = true;
+    let my_grade = 'A';
+
+    let num_1: f32 = 1.111111111111111;
+    let num_2: f64 = 1.111111111111111;
+    println!("f32: {}", num_1 + 0.111111111111111);
+    println!("f64: {}", num_2 + 0.111111111111111);
+
+    let mut num_3: u32 = 5;
+    let num_4: u32 = 4;
+    println!("5 + 4 = {}", num_3 + num_4);
+    println!("5 - 4 = {}", num_3 - num_4);
+    println!("5 * 4 = {}", num_3 * num_4);
+    println!("5 / 4 = {}", num_3 / num_4);
+    println!("5 % 4 = {}", num_3 % num_4);
+    num_3 += 1;
+}
+
+fn sample3_main() {
+    println!("Max u32: {}", u32::MAX);
+    println!("Max u64: {}", u64::MAX);
+    println!("Max usize: {}", usize::MAX);
+    println!("Max u128: {}", u128::MAX);
+    println!("Max f32: {}", f32::MAX);
+    println!("Max f64: {}", f64::MAX);
+}
+
+fn sample2_main() {
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    let age: &str = "47";
+    let mut age: u32 = age.trim().parse()
+        .expect("Age wasn't assigned a number");
+    age = age + 1;
+    println!("I am {} and I want ${}", age, ONE_MIL);
+}
+
+fn sample1_main() {
     println!("Waht is your name?");
     let mut name = String::new();
     let greeting: &str = "Nice to meet you";
